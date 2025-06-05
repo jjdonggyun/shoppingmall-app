cae006160b6c8b15685b2a13aabf6a47bb6947c7
jjdonggyun <a4942963@naver.com>
2025-06-05 21:53:33 +0900

    static middleware setting

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 // 클라이언트에서 API 요청을 보낼 때 도착하는 엔트리 파일 / 노드 js의 진입점이 되는 파일
 const express = require('express');
+// 절대경로 사용
+const path = require('path');
 
 const app = express();
 const port = 4000;
@@ -7,6 +9,9 @@ const port = 4000;
 app.get('/', (req, res) => {
     res.send('안녕하세요. 1111')
 })
+
+// console.log(path.join(__dirname, '../uploads'));
+app.use(express.static(path.join(__dirname, '../uploads')));
 
 app.listen(port, () => {
     console.log(`${port}번에서 실행이 되었습니다.`)
