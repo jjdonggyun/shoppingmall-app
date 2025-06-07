8db9d868170ca07081d3e41539b40f76766bd381
jjdonggyun <a4942963@naver.com>
2025-06-07 23:48:44 +0900

    register router create

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,7 +34,7 @@ app.post('/', (req, res) => {
     res.json(req.body);
 })
 
-// app.use('/users', require('./routes/users'));
+app.use('/users', require('./routes/users'));
 
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
