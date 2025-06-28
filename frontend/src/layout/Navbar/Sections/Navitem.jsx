1fe6587d02dc3821f99e94b06f053b80b2cc9d9a
jjdonggyun <a4942963@naver.com>
2025-06-29 00:17:10 +0900

    NavBar add

--- frontend/src/layout/Navbar/Sections/Navitem.jsx
+++ frontend/src/layout/Navbar/Sections/Navitem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navitem = () => {
   return (
     <div>
-      
+      NavItem
     </div>
   )
 }
--- frontend/src/layout/Navbar/index.jsx
+++ frontend/src/layout/Navbar/index.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import Navitem from './Sections/Navitem';
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
+  const handleMenu = () => {
+    setMenu(!menu);
+  }
+
   return (
-    <div>
-      Navbar
-    </div>
+    <section className='relative z-10 text-white bg-gray-900'>
+      <div className='w-full'>
+        <div className='flex items-center justify-between mx-5 sm:mx-10 lg:mx-20'>
+          {/* logo */}
+          <div className='flex items-center text-2xl h-14'>
+            <Link to="/">Logo</Link>
+          </div>
+
+          {/* menu button */}
+          <div className='text-2xl sm:hidden'>
+            <button onClick={handleMenu}>{menu ? "-" : "+"}</button>
+          </div>
+
+          {/* big screen nav-items */}
+          <div className='hidden sm:block'>
+            <Navitem />
+          </div>
+
+        </div>
+
+        {/* mobile nav-items */}
+        <div className='block sm:hidden'>
+          {menu && <Navitem />}
+        </div>
+      </div>
+    </section>
   )
 }
 
